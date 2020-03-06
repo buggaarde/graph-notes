4e0ad73ec3ff8e3badb2d7c21234252f4fffef57
Simon Bugge Siggaard <buggaarde@gmail.com>
2020-03-06 14:13:11 +0100

    create expression that displays the output of grep for current file

--- graph-notes.el
+++ graph-notes.el
@@ -1,3 +1,5 @@
+(require 's)
+
 (define-minor-mode graph-notes-mode
   "Organize your notes in a graph"
   :lighter " graph-notes")
@@ -5,3 +7,9 @@
 (define-minor-mode gn-mode
   "The minor mode of the graph-notes links buffer"
   :lighter " gn")
+
+
+(let ((b-name (car (s-split "[.]" (buffer-name)))))
+  (let ((buffer (generate-new-buffer (s-lex-format "in-links<${b-name}>"))))
+	(shell-command (s-lex-format "grep -nwrH -C1 \"${b-name}\" .") buffer)
+	(pop-to-buffer buffer)))
