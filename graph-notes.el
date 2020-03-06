f5b91373e3e19b4854ab99b085308c3ab4d3aac7
Simon Bugge Siggaard <buggaarde@gmail.com>
2020-03-06 12:35:46 +0100

    Initial commit

---
+++ graph-notes.el
@@ -0,0 +1,7 @@
+(define-minor-mode graph-notes-mode
+  "Organize your notes in a graph"
+  :lighter " graph-notes")
+
+(define-minor-mode gn-mode
+  "The minor mode of the graph-notes links buffer"
+  :lighter " gn")
